3af46efefa818958ddb154027bb521257564a44e
c6h5ona <c6h5ona@fe84a142-0885-5ed3-d2ee-e6631bea406d>
2010-05-25 07:24:18 +0000

    project of android Trojan.

---
+++ src/com/info_leaker/main_activity.java
@@ -0,0 +1,13 @@
+package com.info_leaker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class main_activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
